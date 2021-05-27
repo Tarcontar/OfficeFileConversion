e6eec367d5fe71a639cd45e0225cff71cb873571
Michael Schnabel <michael-schnabel@gmx.net>
2021-05-27 21:20:09 +0200

    changed paths

--- convert_files.py
+++ convert_files.py
@@ -16,9 +16,9 @@ from multiprocessing import Process
 process_malicious = True #if len(sys.argv) >= 3 and sys.argv[2] in ['True', 'true'] else False
 
 source = sys.argv[1]
-issue_target_dir = 'X:\\ZZ\\IF'
-legacy_target_dir = 'X:\\ZZ\\BF'
-logfile = open('X:\\ZZ\\log.txt', 'a')
+issue_target_dir = 'C:\\IF'
+legacy_target_dir = 'C:\\BF'
+logfile = open('C:\\log.txt', 'a')
 
 ACCESS_DENIED = 5
 
