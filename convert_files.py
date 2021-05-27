6e49023d7893949ffbc6f43a8e0dc9f0b2a8a0ae
Michael Schnabel <michael-schnabel@gmx.net>
2021-05-27 21:46:21 +0200

    small modification

--- convert_files.py
+++ convert_files.py
@@ -196,7 +196,7 @@ def process_outlook(word, excel, ppt, outlook, source):
         doc.ExportAsFixedFormat(source[:-4] + '.pdf', 17)
         doc.Close(False)
         os.remove(html_path)
-        shutil.rmtree(source[:-4] + '_files')
+        shutil.rmtree(source[:-4] + '-Dateien')#'_files')
         
         if not msg.Attachments:
             return
@@ -422,7 +422,7 @@ if __name__ == "__main__":
         
         
     zipping_workers = []
-    for i in range(0, 10):
+    for i in range(0, 1):
         thread = threading.Thread(target=zipping_worker, args=())
         thread.daemon = True
         thread.start()
